bbd02b7f104019f4a95ecacf9c87f396e4d80a29
Bashayar <bashayar@Bashayars-MacBook-Air.local>
2020-04-21 23:43:22 +0100

    add / back

--- api/connection.py
+++ api/connection.py
@@ -10,4 +10,4 @@ connection = pymysql.connect(host='127.0.0.1',
                            user= db_user,
                             password= db_pass,
                             db= db_name,
-                            unix_socket="cloudsql/{}".format(cloud_sql_connection_name))
+                            unix_socket="/cloudsql/{}".format(cloud_sql_connection_name))
