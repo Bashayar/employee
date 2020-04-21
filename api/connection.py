b9f97529268c1f5a66236a33289f09b3f158f9c6
Bashayar <bashayar@Bashayars-MacBook-Air.local>
2020-04-22 00:20:27 +0100

    remove unix socet

--- api/connection.py
+++ api/connection.py
@@ -1,6 +1,5 @@
 import pymysql
 
-
 db_user = "frontend"
 db_pass = "frontend"
 db_name = "ccdatabase"
@@ -10,4 +9,4 @@ connection = pymysql.connect(host='127.0.0.1',
                            user= db_user,
                             password= db_pass,
                             db= db_name,
-                            unix_socket="/cloudsql/{}".format(cloud_sql_connection_name))
+                            )
